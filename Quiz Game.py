31a5f3015a612bef1779ab3e17b3d94bfb0c1f44
rmcstlln <romarkpinero68@gmail.com>
2022-11-19 20:55:17 +0800

    copied user answer 10x

--- Quiz Game.py
+++ Quiz Game.py
@@ -23,6 +23,7 @@ print("a. Ribonucleic acid")
 print("b. Deoxyribonulic acid")
 print("c. Folic acid")
 print("d. None of these")
+answer = input("What is your answer? ")
 
 # Question 3
 print("3. What is length?")
@@ -30,6 +31,7 @@ print("a. The distance between 100 points")
 print("b. The distance between 4 points")
 print("c. The distance between 1000 points")
 print("d. The distance between 2 points")
+answer = input("What is your answer? ")
 
 # Question 4
 print("4. The concept of gravity was discovered by which famous physicist?")
@@ -37,6 +39,7 @@ print("a. Galileo Galilei")
 print("b. Robert Boyle")
 print("c. Isaac Newton")
 print("d. Leonardo da Vinci")
+answer = input("What is your answer? ")
 
 # Question 5
 print("5. You see your reflection in a mirror because?")
@@ -44,6 +47,7 @@ print("a. light is absorbed")
 print("b. light is diffracted")
 print("c. light is reflected")
 print("d. light is refracted")
+answer = input("What is your answer? ")
 
 # Question 6
 print("6. Which of these have positive charge and are found in the nucleus of an atom")
@@ -51,6 +55,7 @@ print("a. Neutrons")
 print("b. Protons")
 print("c. Electrons")
 print("d. Elements")
+answer = input("What is your answer? ")
 
 # Question 7
 print("7. One complete revolution of earth around the sun is equal to approximately")
@@ -58,6 +63,7 @@ print("a. Year")
 print("b. Month")
 print("c. Week")
 print("d. Day")
+answer = input("What is your answer? ")
 
 # Question 8
 print("8. Which one of these isn't an organ in the human body?")
@@ -65,6 +71,7 @@ print("a. Heart")
 print("b. Kidney")
 print("c. Brain")
 print("d. Spatula")
+answer = input("What is your answer? ")
 
 # Question 9
 print("9. How many bones are there in an adult human body?")
@@ -72,6 +79,7 @@ print("a. 36")
 print("b. 98")
 print("c. 206")
 print("d. 872")
+answer = input("What is your answer? ")
 
 # Question 10
 print("10. What do Venus and Mercury have in common?")
@@ -79,3 +87,4 @@ print("a. Their atmospheres are identical")
 print("b. They don't have moons")
 print("c. They have nothing in common")
 print("d. Their surface temperature are exactly the same")
+answer = input("What is your answer? ")

8284a445438affa90a8f683836dbe8331fa0d2e4
rmcstlln <romarkpinero68@gmail.com>
2022-11-19 20:33:51 +0800

    copied question 1 with choices 10x

--- Quiz Game.py
+++ Quiz Game.py
@@ -15,3 +15,66 @@ print("a. Umbra")
 print("b. Penumbra")
 print("c. Hyperumbra")
 print("d. None of these")
+
+# Question 2
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 3
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 4
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 5
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 6
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 7
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 8
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 9
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
+
+# Question 10
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")

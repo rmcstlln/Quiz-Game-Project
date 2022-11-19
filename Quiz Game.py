e484001f14bc16e1577b2d14c320626f8c224d17
rmcstlln <romarkpinero68@gmail.com>
2022-11-19 20:31:25 +0800

    displayed question 1 with choices

--- Quiz Game.py
+++ Quiz Game.py
@@ -8,3 +8,10 @@ if play.upper() != 'Y':
     quit()
 
 score = 0
+
+# Question 1
+print("1. The darkest part of the shadow is")
+print("a. Umbra")
+print("b. Penumbra")
+print("c. Hyperumbra")
+print("d. None of these")
